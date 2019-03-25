8baa9d4401a5ed2b1cf1e0d06a00635cc0db166d
voceperdeuseutempolendoisso <44319364+voceperdeuseutempolendoisso@users.noreply.github.com>
2019-03-25 16:38:33 -0300

    Update mandelbrot.jl

--- mandelbrot.jl
+++ mandelbrot.jl
@@ -3,11 +3,10 @@
 
 using Images
 
-# z^2 = x^2 + 2xyi - y^2 = (x^2 - y^2) + (2xy) i
 function iteracao_mandelbrot(cx, cy; max_iter = 40)
   iter = 0
   x = y = 0
-  while iter<40 && x^2+y^2<4
+  while iter<40
     real = (x^2 - y^2) + cx
     imaginaria = (2*x*y) + cy
     x = real
@@ -36,7 +35,7 @@ function mandelbrot(N)
     end
   end
 
-  save("mandelbrot.jpg", imagem')
+  save("paulo.jpg", imagem')
 end
 
 mandelbrot(1000)
